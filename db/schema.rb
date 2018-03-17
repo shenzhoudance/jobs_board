1b7c728a830a299e111345a862275dd6bc242b86
xiaowei <shenzhoudance@gmail.com>
2018-03-17 16:11:29 +0800

    add Category image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317073839) do
+ActiveRecord::Schema.define(version: 20180317074737) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180317073839) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
