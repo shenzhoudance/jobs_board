e86f94007ee10d14037c2c680ee1bf1b4dac1ce9
xiaowei <shenzhoudance@gmail.com>
2018-03-17 11:22:06 +0800

    edit simple

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,41 @@
 class JobsController < ApplicationController
-  def index
+
+ def index
  end
+
+ def show
+ end
+
+def new
+  @job = Job.new
+end
+
+ def create
+   @job = Job.new(jobs_params)
+   if @job.save
+     redirect_to @jobs
+   else
+     render "New"
+   end
+ end
+
+ def edit
+ end
+
+ def update
+ end
+
+ def destory
+ end
+
+ private
+
+ def jobs_params
+   params.require(:job).permit(:title,:description,:company,:url)
+ end
+
+ def find_job
+   @job = Job.find(params[:id])
+ end
+
 end
